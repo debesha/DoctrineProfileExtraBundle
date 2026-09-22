d2054713fa8344610a33855f54d8da0ce7c6a95e
Dima Malyshenko <dima@malyshenko.com>
2026-09-22 17:54:20 +0100

    Use DI component Extension base class (fixes #41)

--- DependencyInjection/DebeshaDoctrineProfileExtraExtension.php
+++ DependencyInjection/DebeshaDoctrineProfileExtraExtension.php
@@ -4,8 +4,8 @@ namespace Debesha\DoctrineProfileExtraBundle\DependencyInjection;
 
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Extension\Extension;
 use Symfony\Component\DependencyInjection\Loader;
-use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 
 /**
  * This is the class that loads and manages your bundle configuration.
